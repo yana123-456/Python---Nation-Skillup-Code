54585abb6bef5c3b3a97b75878e58d3bba53b43d
Yana Srivastava <76560682+yana123-456@users.noreply.github.com>
2025-08-24 23:46:39 +0530

    Add files via upload

---
+++ Day 2 Questions/comparison.py
@@ -0,0 +1,9 @@
+#Code to implement comparison operation on integers
+num1 = 30
+num2 = 35
+if num1 > num2:
+    print("The first number is greater.")
+elif num2 > num1:
+    print("The second number is greater.")
+else:
+    print("Both the numbers are equal.")
