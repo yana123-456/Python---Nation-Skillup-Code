cf20d0bc699f6f6bc018b9f3b87121e5d5188ae4
Yana Srivastava <yanusrivastava@gmail.com>
2025-08-26 01:10:21 +0530

    added arithmetic.py to .

---
+++ Day 2 Problems/arithmetic.py
@@ -0,0 +1,20 @@
+"""You are given two integer variables x and y. You need to perform the following operations:
+
+p = x + y : Addition
+q = x - y : Subtraction
+r = x * y :Multiplication
+s = x / y : Division
+t = x % y : Modulo"""
+
+x = int(input("Enter the first number:"))
+y = int(input("Enter thr second number:"))
+p = x + y
+q = x - y
+r = x * y
+s = x / y
+t = x % y
+print("Sum:", p)
+print("Difference:", q)
+print("Multiplication:",r)
+print("Division:", s)
+print("Remainder:", t)
