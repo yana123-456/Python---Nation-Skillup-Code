5d70f9e3f2856203de2073b473ada19c20aab054
Yana Srivastava <yanusrivastava@gmail.com>
2025-08-26 00:35:58 +0530

    added login.py to Day 2 Questions

---
+++ Day 2 Questions/login.py
@@ -0,0 +1,11 @@
+#Code to logoin using username and password
+username = input("Enter the username:")
+password = input("Enter the password:")
+if username == "admin":
+    if password == "1234":
+        print("Access Granted")
+    else:
+        print("Incorrect Password")
+else:
+    print("Username not found")
+    
