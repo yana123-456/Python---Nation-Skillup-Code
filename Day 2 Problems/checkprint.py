832d5eaeab5eb06f2d8d3ad93640dfad76ed2f7a
Yana Srivastava <yanusrivastava@gmail.com>
2025-08-28 00:14:18 +0530

    added checkprint.py to .

---
+++ Day 2 Problems/checkprint.py
@@ -0,0 +1,9 @@
+"""Given an integer a, you have to use the if statement to print "Big" (without quotes) if the given number is greater than 100, and use the else statement to print "Number" (without quotes) when the number is smaller than or equal to 100.
+
+Note: After printing the output, you should move the cursor to the new line."""\
+    
+a = int(input("Enter the number:"))
+if a > 100:
+    print("Big")
+else:
+    print("Number")
